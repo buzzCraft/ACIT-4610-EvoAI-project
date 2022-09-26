5e304576ed54938307627ec8a00d444fdbbe025c
buzzCraft <67235613+buzzCraft@users.noreply.github.com>
2022-09-26 13:54:10 +0200

    Added main, spikeGenerator and a test file

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import random
 from keras.datasets import mnist
 from matplotlib import pyplot as plt
-from pylab import *
+# from pylab import *
 import numpy as np
-import pycxsimulator
+import spikeGen
 
 # Image set
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
-# Get total pixels in an image
-PIX = train_X.shape[1] * train_X.shape[2]
+
 
 
 # Neural spiking network
 class Neuron:
-    def __init__(self, id, threshold, weight, leakage):
+    def __init__(self, id, threshold, leakage, weight):
         self.id = id
         self.threshold = threshold
         self.weight = weight
@@ -21,13 +20,13 @@ class Neuron:
         self.fired = False
         self.spike = 0
         self.leakage = leakage
-        self.connections = []
+        # self.connections = []
 
 
     # Function to fire the neuron
     def fire(self):
         self.fired = True
-        self.spike = .5 * self.weight
+        self.spike = 1 #TODO endres til 1 eller noe sånn etterhvet
         self.potential = 0
 
     # Function to reset the neuron
@@ -35,166 +34,196 @@ class Neuron:
         self.fired = False
         self.spike = 0
 
+
     # Function to update the neuron
     # If the neuron has fired, reset it
     # If the neuron has not fired, update its potential with leakage
     def update(self, input):
-        # had += input * self.weight, but moved weight to spike in def fire()
-        self.potential += (input * self.weight) - self.leakage*self.potential
-        # Just for testing
-        pot = self.potential
-        if self.potential < 0: self.potential = 0
+
+        self.potential += (input) - self.leakage*self.potential #(input * self.weight) - self.leakage*self.potential
+
         if self.potential >= self.threshold:
             self.fire()
         else:
             self.reset()
-        # Returning potential for testing
-        return pot
 
     # Get the output of the neuron
     def get_spike(self):
         return self.spike
+
+    def get_spike_and_weight(self, next_neuron):
+        return self.spike, self.weight[next_neuron.id]
 
     def get_potential(self):
         return self.potential
 
 
 class Layer:
+    """
+    Base class for Layers
+    """
     def __init__(self):
         self.neurons = []
 
+    def get_spikes(self):
+        """
+        :return: list of spikes
+        """
+        return [n.get_spike() for n in self.neurons]
+    def get_weights(self, id):
+        """
+        :return: list of weights
+        """
+        return [n.weight[id] for n in self.neurons]
 
 
 
 
-# TEST METHODS
-def test_neuron():
-    threshold=1.5
-    n1 = Neuron(1, threshold, 1, 0.1)
-    response_list = []
-    potential_list = []
-
-    spike_list = [0,0,1,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0]
-    for element in spike_list:
-        potential_list.append(n1.update(element))
-        # potential_list.append(n1.get_potential())
-        response_list.append(n1.get_spike())
-
-    plt.plot(response_list, label='Spike')
-    plt.plot(spike_list, label='Input')
-    plt.plot(potential_list, label='Potential')
-    plt.axhline(y=threshold, color='r', linestyle='-')
-    plt.legend()
-    plt.show()
+class inputLayer(Layer):
+    """
+    Input layer with a update function that takes a list of inputs
+    """
+    def __init__(self):
+        super().__init__()
+    # Take a list of inputs (one for each layer and update the neurons)
+    def update(self, input_list):
+        for i in range(len(self.neurons)):
+            self.neurons[i].update(input_list[i])
 
 
 
-def test_layer():
-    inputLayer = Layer()
-    for i in range(2):
-        inputLayer.neurons.append(Neuron(id=i, threshold=.8, weight=0.5, leakage=.01))
+class hiddenLayer(Layer):
+    """
+    Hidden layer with a update function that takes the previous layer as input
+    """
+    def __init__(self, prev_layer):
+        super().__init__()
+        self.prev_layer = prev_layer
+
+    # Take the previous layer as input and update the neurons
+    def update(self):
+        for n in self.neurons:
+            # Get the spikes from the previous layer for current neuron
+            weights = self.prev_layer.get_weights(n.id)
+            spikes = self.prev_layer.get_spikes()
+            # Sum the weighted spikes from the previous layer for the current neuron
+            n.update(sum([w*s for w,s in zip(weights, spikes)]))
+            # print(sum([w*s for w,s in zip(weights, spikes)]))
+            # Means the same as:
+            # [1,3,4] [2,4,5] -> 1*2 + 3*4 + 4*5 = 26
+
+
+class outputLayer(Layer):
+    """
+    Output layer with a update function that takes the previous layer as input
+    And has a get_output function that returns the output of the layer
+    """
+    def __init__(self, prev_layer):
+        super().__init__()
+        self.prev_layer = prev_layer
+
+    # Take the previous layer as input and update the neurons
+    def update(self):
+        for n in self.neurons:
+            # Get the spikes from the previous layer for current neuron
+            weights = self.prev_layer.get_weights(n.id)
+            spikes = self.prev_layer.get_spikes()
+            # Sum the weighted spikes from the previous layer for the current neuron
+            n.update(sum([w*s for w,s in zip(weights, spikes)]))
+            pot = n.get_potential()
+        print(self.get_output())
+
+
+
+
+    def get_output(self):
+        return [n.get_spike() for n in self.neurons]
+
+
+class Network:
+    """
+    A class to represent a network
+    Calling update will update each layer in the network
+    """
+    def __init__(self, input_l, hidden_l, output_l):
+        self.inputLayer = input_l
+        self.hiddenLayer = hidden_l
+        self.outputLayer = output_l
+
+    def update(self, input):
+        self.inputLayer.update(input)
+        self.hiddenLayer.update()
+        self.outputLayer.update()
+
+    def observe(self):
+        pass
+
+
+
+
+
+
+
+
+
+def initialize(nr_input, nr_hidden, nr_output, threshold = 1.5, leakage = 0.01):
+    """
+    :param nr_input: number of input neurons
+    :param nr_hidden: number of hidden neurons
+    :param nr_output: number of output neurons
+    :param threshold: threshold for firing
+    :param leakage: leakage for updating potential
+    """
+    global network
+
+    input_layer = inputLayer()
+    for i in range(nr_input):
+        # Generate a list with length of nr_hidden, with random values between 0 and 1
+        weight_list = [random.random() for _ in range(nr_hidden)]
+        # The line above is the same as:
+        # weight_list = []
+        # for i in range(nr_hidden):
+        #     weight_list.append(random.random())
+        input_layer.neurons.append(Neuron(id=i, threshold=threshold, weight=weight_list, leakage=leakage))
     # Creating hidden layer
-    hiddenLayer = Layer()
-    for i in range(1):
-        hiddenLayer.neurons.append(Neuron(id=i, threshold=.6, weight=.5, leakage=.05))
-
-    response_list = []
-    potential_list = []
-    l1 = []
-    l2 = []
-
-    spike_list1 = [0,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0]
-    spike_list2 = [0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1]
-    # spike_list2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    for i in range(len(spike_list1)):
-        inputLayer.neurons[0].update(spike_list1[i])
-        inputLayer.neurons[1].update(spike_list2[i])
-        l = inputLayer.neurons[0].get_spike()
-        l1.append(l)
-        u = inputLayer.neurons[1].get_spike()
-        l2.append(u)
-        potential_list.append(hiddenLayer.neurons[0].update( u+l ))
-        # potential_list.append(n1.get_potential())
-        response_list.append(hiddenLayer.neurons[0].get_spike())
-
-    plt.plot(response_list, label='Spike')
-    plt.plot(spike_list1, label='Input1')
-    plt.plot(spike_list2, label='Input2')
-    plt.plot(l1, label='1 pot')
-    plt.plot(l2, label='2 pot')
-    plt.plot(potential_list, label='Potential')
-    plt.axhline(y=0.6, color='r', linestyle='-')
-    plt.legend()
-    plt.show()
-
-
-def initialize():
-    global spike_train, inputLayer, hiddenLayer, outputLayer
-    spike_train = np.zeros((PIX, 1))
-    inputLayer = Layer()
-    for i in range(PIX):
-        inputLayer.neurons.append(Neuron(id=i, threshold=.5, weight=.5, leakage=.01))
-    # Creating hidden layer
-    hiddenLayer = Layer()
-    for i in range(20):
-        hiddenLayer.neurons.append(Neuron(id=i, threshold=.5, weight=.5, leakage=.01))
+    hidden_layer = hiddenLayer(input_layer)
+    for i in range(nr_hidden):
+        # Generate a list with length of nr_output, with random values between 0 and 1
+        weight_list = [random.random() for _ in range(nr_output)]
+        hidden_layer.neurons.append(Neuron(id=i, threshold=threshold, weight=weight_list, leakage=leakage))
     # Creating output layer
-    outputLayer = Layer()
-    for i in range(10):
-        outputLayer.neurons.append(Neuron(id=i, threshold=.5, weight=.5, leakage=.01))
+    output_layer = outputLayer(hidden_layer)
+    for i in range(nr_output):
+        output_layer.neurons.append(Neuron(id=i, threshold=threshold, weight=[1], leakage=leakage))
 
     # Creating network
-    network = [inputLayer, hiddenLayer, outputLayer]
-    # Creating connections between layers
-    for i in range(len(inputLayer.neurons)):
-        for j in range(len(hiddenLayer.neurons)):
-            inputLayer.neurons[i].weight = np.random.uniform(0, 1)
-            hiddenLayer.neurons[j].weight = np.random.uniform(0, 1)
-            inputLayer.neurons[i].connections.append(hiddenLayer.neurons[j])
-            hiddenLayer.neurons[j].connections.append(inputLayer.neurons[i])
-
-    for i in range(len(hiddenLayer.neurons)):
-        for j in range(len(outputLayer.neurons)):
-            hiddenLayer.neurons[i].weight = np.random.uniform(0, 1)
-            outputLayer.neurons[j].weight = np.random.uniform(0, 1)
-            hiddenLayer.neurons[i].connections.append(outputLayer.neurons[j])
-            outputLayer.neurons[j].connections.append(hiddenLayer.neurons[i])
+    network = Network(input_layer, hidden_layer, output_layer)
+    return network
 
 
 def observe():
     pass
 
 def update():
-    #Send one bit of spike_train to each neuron in input layer
-    for i in range(PIX):
-        inputLayer.neurons[i].update(spike_train[i])
+    pass
+
+
+if __name__ == '__main__':
+    # Get total pixels in an image
+    nr_pix = train_X.shape[1] * train_X.shape[2]
+    initialize(nr_pix, 20, 10)
+
+
+    # Create a spiketrain for a image
+    spikeTrain = spikeGen.rateCodingRand2D(train_X[0], T = 50)
+    num_rows, num_cols = spikeTrain.shape
+    for i in range(num_cols):
+        inp = (spikeTrain[:,i])
+        network.update(inp)
 
 
 
-test_layer()
-test_neuron()
-# pycxsimulator.GUI().start(func=[initialize, observe, update])
-initialize()
-# Creating input layer
 
 
 
 
-
-
-# for i in range(9):
-#     pyplot.subplot(330 + 1 + i)
-#     pyplot.imshow(train_X[i], cmap=pyplot.get_cmap('gray'))
-# pyplot.show()
-
-
-# for x in range(10):
-#     for i in range(50000):
-#         if train_y[i]==x:
-#             plt.imshow(train_X[i], cmap=plt.get_cmap('gray'), alpha=0.1)
-#     plt.show()
-
-
-# Class
-# 0 - low / no spikes - low frequency
-# 255 - high / many spikes - high frequency
---
+++ tests.py
@@ -0,0 +1,76 @@
+from main import *
+# TEST METHODS
+def test_neuron():
+    threshold=1.5
+    n1 = Neuron(1, threshold=threshold, leakage=.1, weight=[.79])
+    response_list = []
+    potential_list = []
+
+    spike_list = [0,0,1,1,0,0,1,1,1,0,0,0,1,0,0,0,0,0,0,1,1,0,0,1,0,0,0,0,1,0,0]
+    for element in spike_list:
+        potential_list.append(n1.update(element))
+        # potential_list.append(n1.get_potential())
+        response_list.append(n1.get_spike())
+
+    plt.plot(response_list, label='Spike')
+    plt.plot(spike_list, label='Input')
+    plt.plot(potential_list, label='Potential')
+    plt.axhline(y=threshold, color='r', linestyle='-')
+    plt.legend()
+    plt.show()
+
+
+
+def test_layer():
+    inputLayer = Layer()
+    for i in range(2):
+        inputLayer.neurons.append(Neuron(id=i, threshold=.8, weight=[0.5], leakage=.01))
+    # Creating hidden layer
+    hiddenLayer = Layer()
+    for i in range(1):
+        hiddenLayer.neurons.append(Neuron(id=i, threshold=.6, weight=[.5], leakage=.05))
+
+    response_list = []
+    potential_list = []
+    l1 = []
+    l2 = []
+
+    spike_list1 = [0,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0]
+    spike_list2 = [0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1]
+    # spike_list2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    for i in range(len(spike_list1)):
+        inputLayer.neurons[0].update(spike_list1[i])
+        inputLayer.neurons[1].update(spike_list2[i])
+        l = inputLayer.neurons[0].get_spike()
+        l1.append(l)
+        u = inputLayer.neurons[1].get_spike()
+        l2.append(u)
+        potential_list.append(hiddenLayer.neurons[0].update( u+l ))
+        # potential_list.append(n1.get_potential())
+        response_list.append(hiddenLayer.neurons[0].get_spike())
+
+    plt.plot(response_list, label='Spike')
+    plt.plot(spike_list1, label='Input1')
+    plt.plot(spike_list2, label='Input2')
+    plt.plot(l1, label='1 pot')
+    plt.plot(l2, label='2 pot')
+    plt.plot(potential_list, label='Potential')
+    plt.axhline(y=0.6, color='r', linestyle='-')
+    plt.legend()
+    plt.show()
+
+if __name__ == '__main__':
+    # test_neuron()
+    # test_layer()
+    #
+    # # Spike train testing
+    # spikeTrain = spikeGen.rateCodingRand2D(train_X[0], T=15)
+    # print("done")
+
+    # some numpy testing
+
+    arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
+    print(arr)
+    num_rows, num_cols = arr.shape
+    for i in range(num_cols):
+        print(arr[:,i])
